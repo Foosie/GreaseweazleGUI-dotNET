ef15d870fe479e464c8b16e44fb43633b3f671f2
Don Mankin <don@desertsagesolutions.com>
2020-03-17 09:19:28 -0700

    Minor Edits

--- VS 2015 Solution/Greaseweasle/ChooserForm.cs
+++ VS 2015 Solution/Greaseweasle/ChooserForm.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿// ChooserForm.cs
+//
+// Greaseweazle GUI Wrapper
+//
+// Copyright (c) 2019 Don Mankin <don.mankin@yahoo.com>
+//
+// MIT License
+//
+// See the file LICENSE for more details, or visit <https://opensource.org/licenses/MIT>.
+
+using System;
 using System.IO;
 using System.Diagnostics;
 using System.Windows.Forms;
