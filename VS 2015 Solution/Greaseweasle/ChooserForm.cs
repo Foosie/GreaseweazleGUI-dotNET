c1c1162f8e8e377a6c15dda2d1cd92611806ca7c
Don Mankin <don@desertsagesolutions.com>
2020-07-12 06:41:35 -0700

    Auto switch to EXE mode

--- VS 2015 Solution/Greaseweasle/ChooserForm.cs
+++ VS 2015 Solution/Greaseweasle/ChooserForm.cs
@@ -18,6 +18,7 @@ using System.Threading;
 using System.Linq;
 using System.IO.Ports;
 using System.Globalization;
+using System.Timers;
 
 namespace Greaseweazle
 {
@@ -168,8 +169,19 @@ namespace Greaseweazle
             // read our settings
             iniReadFile();
 
+            // if compiled windows version check it
+            if (File.Exists(sExeDir + "\\gw.exe"))
+            {
+                if (!mnuWindowsEXE.Checked)
+                {
+                    AutoClosingMessageBox.Show("Found gw.exe in folder.\n\nWindows Self-contained Executable mode auto-selected", this.Text, 10000);
+                    mnuWindowsEXE.Checked = true;
+                    m_bWindowsEXE = true;
+                }
+            }
+
             // set default folders
-            if (m_sReadDiskFolder.Length == 0)
+                if (m_sReadDiskFolder.Length == 0)
                 m_sReadDiskFolder = sExeDir;
             if (m_sWriteDiskFolder.Length == 0)
                 m_sWriteDiskFolder = sExeDir;
@@ -801,5 +813,39 @@ namespace Greaseweazle
             MessageBox.Show(m_sInfo);
         }
         #endregion
+
+       
     }
+
+    #region AutoClosingMessageBox
+    public class AutoClosingMessageBox
+    {
+        System.Threading.Timer _timeoutTimer;
+        string _caption;
+        AutoClosingMessageBox(string text, string caption, int timeout)
+        {
+            _caption = caption;
+            _timeoutTimer = new System.Threading.Timer(OnTimerElapsed,
+                null, timeout, System.Threading.Timeout.Infinite);
+            using (_timeoutTimer)
+                MessageBox.Show(text, caption);
+        }
+        public static void Show(string text, string caption, int timeout)
+        {
+            new AutoClosingMessageBox(text, caption, timeout);
+        }
+        void OnTimerElapsed(object state)
+        {
+            IntPtr mbWnd = FindWindow("#32770", _caption); // lpClassName is #32770 for MessageBox
+            if (mbWnd != IntPtr.Zero)
+                SendMessage(mbWnd, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+            _timeoutTimer.Dispose();
+        }
+        const int WM_CLOSE = 0x0010;
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+    }
+    #endregion
 }
